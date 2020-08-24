c751ba23551331817e18925e8cecef3df564c4cc
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-08-24 14:09:39 +0900

    変数への代入をしない形に修正

--- travel-plan.rb
+++ travel-plan.rb
@@ -33,7 +33,7 @@ def plan_select(travel_plans)
 	selected_travel_plan = travel_plans[selected_number -1] 
   puts "#{selected_travel_plan[:place]}旅行が選択されました"
   puts "金額はお一人様#{selected_travel_plan[:price]}円です"
-	selected_price = selected_travel_plan[:price]  #メソッドの最後の値
+	selected_travel_plan[:price]  #メソッドの最後の値
 end
 
 # 人数入力・旅行代金計算
